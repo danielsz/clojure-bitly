fa077350a6c452cc1cc5e8cbf0cbd9c614d44664
Daniel Szmulewicz <daniel.szmulewicz@gmail.com>
2014-06-18 03:32:41 +0300

    clojars setup

--- project.clj
+++ project.clj
@@ -1,12 +1,11 @@
-(defproject clojure-bitly "0.1.4"
+(defproject org.danielsz/clojure-bitly "0.1.4"
   :description "Bit.ly API client for Clojure"
   :url "https://github.com/danielsz/clojure-bitly"
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
                  [clj-http "0.7.7"]
                  [org.clojure/data.json "0.2.3"]]
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dev-dependencies [[lein-clojars "0.6.0"]]
   :repositories [["releases" {:url "https://clojars.org/" :creds :gpg}]
                  ["snapshots" {:url "https://clojars.org/" :creds :gpg}]]
   :scm {:name "git"
